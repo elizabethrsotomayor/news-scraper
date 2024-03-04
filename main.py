c70dbb51bd6941625ae3f2d8fb0102d905d764c2
Elizabeth Sotomayor <elizabethrsotomayor@gmail.com>
2024-03-04 15:55:50 -0600

    add cnn, remove bloomberg

--- main.py
+++ main.py
@@ -9,15 +9,10 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 # driver = webdriver.Chrome(options=chrome_options)
-# driver.get("https://www.theguardian.com")
+# driver.get("https://www.cnn.com")
 #
-# headlines = driver.find_elements(By.CSS_SELECTOR, ".dcr-yyvovz ul")
-
-# print("GUARDIAN HEADLINES:")
-# for headline in headlines:
-#     print(headline.text)
-
-# headlines = [headline.text for headline in headlines]
+# headlines = driver.find_elements(By.CSS_SELECTOR, ".container__headline span")
+# print([headline.text for headline in headlines if len(headline.text) > 20])
 
 # close single/active tab
 # driver.close()
@@ -30,23 +25,20 @@ chrome_options.add_experimental_option("detach", True)
 def getUrls(targeturl):
     driver = webdriver.Chrome(options=chrome_options)
     driver.get("http://www." + targeturl + ".com")
-    # driver.quit()
 
     if targeturl == "apnews":
         global apnews_headlines
         # TODO: Get all headlines since this only finds a few.
-        apnews_headlines = driver.find_elements(By.CSS_SELECTOR, ".PagePromo-title span")
+        apnews_headlines = driver.find_elements(By.CSS_SELECTOR, ".PagePromo-title a span")
         apnews_headlines = [headline.text for headline in apnews_headlines]
     elif targeturl == "reuters":
         global reuters_headlines
         reuters_headlines = driver.find_elements(By.CSS_SELECTOR, ".cluster__stories__WrBsf li a")
         reuters_headlines = [headline.text for headline in reuters_headlines]
-    elif targeturl == "bloomberg":
-        global bloomberg_headlines
-        # TODO: Find out how to get Bloomberg headlines since this won't fetch them.
-        bloomberg_headlines = driver.find_elements(By.CSS_SELECTOR, ".Headline_phoenix__tgVV3 span")
-        bloomberg_headlines = [headline.text for headline in bloomberg_headlines]
-        print(bloomberg_headlines)
+    elif targeturl == "cnn":
+        global cnn_headlines
+        cnn_headlines = driver.find_elements(By.CSS_SELECTOR, ".container__headline span")
+        cnn_headlines = [headline.text for headline in cnn_headlines if len(headline.text) > 20]
     elif targeturl == "theguardian":
         global theguardian_headlines
         theguardian_headlines = driver.find_elements(By.CSS_SELECTOR, ".dcr-yyvovz ul")
@@ -54,14 +46,14 @@ def getUrls(targeturl):
     driver.quit()
 
 
-webPage = ['apnews', 'reuters', 'bloomberg', 'theguardian']
+webPage = ['apnews', 'reuters', 'cnn', 'theguardian']
 for i in webPage:
     getUrls(i)
 
 @app.route("/")
 def hello_world():
     return render_template("index.html", apnews_headlines=apnews_headlines,
-                           reuters_headlines=reuters_headlines, bloomberg_headlines=bloomberg_headlines,
+                           reuters_headlines=reuters_headlines, cnn_headlines=cnn_headlines,
                            theguardian_headlines=theguardian_headlines)
 
 if __name__ == "__main__":
